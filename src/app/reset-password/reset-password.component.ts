d408ae1cf5cd4e976f4e5e498604cfaac4f82542
dvvazhap <dh!vyal@12345>
2018-09-03 07:08:43 +0530

    original code

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -27,49 +27,49 @@ export class ResetPasswordComponent implements OnInit {
     })
   }
 
-  // public resetPassword = function () {
-  //   alert("inside resetPAssword..................................")
-  //   if (this.password.length < 8) {
-  //     this.error = 'Password should be of minimum 8 characters';
-  //     this.msg = '';
-  //   }
-  //   else if (this.password != this.confirm_password) {
-  //     this.error = 'Password and Confirm Password should be same';
-  //     this.msg = '';
-  //   } else {
-  //     this.error = '';
-  //     this.msg = '';
-  //     let password = Md5.hashStr(this.password).toString();
+  public resetPassword = function () {
+    alert("inside resetPAssword..................................")
+    if (this.password.length < 8) {
+      this.error = 'Password should be of minimum 8 characters';
+      this.msg = '';
+    }
+    else if (this.password != this.confirm_password) {
+      this.error = 'Password and Confirm Password should be same';
+      this.msg = '';
+    } else {
+      this.error = '';
+      this.msg = '';
+      let password = Md5.hashStr(this.password).toString();
 
-  //     this.serverdata.resetPassword(this.ee, this.rr, password).subscribe(data => {
-  //       alert("data :"+data + this);
-  //       if (data == "mismatch") {
-  //         alert("Invalid link. Click on Forgot password to reset again.");
-  //         this.router.navigate(['/']);
-  //       }
-  //       else if (data == "0") {
-  //         this.msg = "";
-  //         this.password = "";
-  //         this.confirm_password = "";
-  //         this.error = "Reset Password Unsuccessful.";
-  //       }
-  //       else if (data == "1") {
-  //         this.error = "";
-  //         this.password = "";
-  //         this.confirm_password = "";
-  //         window.alert("Reset Password Successful");
-  //         this.router.navigate(['/']);
-  //       }
-  //     }, error => {
-  //       this.msg = "";
-  //       this.error = "Could not verify due to some issues.Please report this issue";
-  //     });
-  //   }
+      this.serverdata.resetPassword(this.ee, this.rr, password).subscribe(data => {
+        alert("data :"+data + this);
+        if (data == "mismatch") {
+          alert("Invalid link. Click on Forgot password to reset again.");
+          this.router.navigate(['/']);
+        }
+        else if (data == "0") {
+          this.msg = "";
+          this.password = "";
+          this.confirm_password = "";
+          this.error = "Reset Password Unsuccessful.";
+        }
+        else if (data == "1") {
+          this.error = "";
+          this.password = "";
+          this.confirm_password = "";
+          window.alert("Reset Password Successful");
+          this.router.navigate(['/']);
+        }
+      }, error => {
+        this.msg = "";
+        this.error = "Could not verify due to some issues.Please report this issue";
+      });
+    }
 
-  // }
+  }
 
-  // public login() {
-  //   this.router.navigate(['/']);
-  // }
+  public login() {
+    this.router.navigate(['/']);
+  }
 
 }
