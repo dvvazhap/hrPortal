16e4d02bde00354104cd54d0b5f5ed2501308d58
dvvazhap <dh!vyal@12345>
2018-09-07 20:45:52 +0530

    adding job sharing

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../services/login.service';
 import { ServerService } from '../services/server.service';
 import { ActivatedRoute } from '@angular/router';
+import { OpeningInfo } from '../interface';
 
 @Component({
   selector: 'app-job',
@@ -10,30 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class JobComponent implements OnInit {
   id: string;
-  requirement = {
-    ind: 0,
-    fullTime: 1,
-    partTime: 0,
-    intern: 0,
-    designation: "",
-    company: "",
-    location: "",
-    contact: "",
-    min_years: 0,
-    max_years: 0,
-    skills: "",
-    specificReq: "",
-    noticePeriod: 0,
-    gender: 'Anyone',
-    count: 1
-  } as any;
-  constructor(private route: ActivatedRoute, private info: LoginService, private serverdata: ServerService) { }
+  valid: number;
+  requirement: OpeningInfo = {} as any;
+  constructor(private route: ActivatedRoute, private serverdata: ServerService) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => { this.id = params['id']; });
-
-    this.serverdata.getOpeningsById(this.id).subscribe(data => {
-      this.requirement = JSON.parse(data);
+    this.valid = 0;
+    this.serverdata.getOpenings("",this.id).subscribe(data => {
+      if(data !='0'){
+        this.valid = 1;
+        this.requirement = JSON.parse(data)[0];
+      }
     })
   }
 }
