8fba199e80983911ce2258a1acd91b98df706d2a
dvvazhap <dh!vyal@12345>
2018-10-02 07:46:52 +0530

    verification code optimised

--- src/app/verify/verify.component.ts
+++ src/app/verify/verify.component.ts
@@ -12,8 +12,7 @@ export class VerifyComponent implements OnInit {
   tt: string;
   rr: number;
   email: string;
-  error:string;
-  info:string;
+  error: string;
   constructor(private route: ActivatedRoute, private router: Router, private serverdata: ServerService, private login_info: LoginService) { }
 
   ngOnInit() {
@@ -28,34 +27,21 @@ export class VerifyComponent implements OnInit {
     this.email = tt.substring(13);
     this.serverdata.verifyUser(tt, rr, this.email).subscribe(data => {
       let temp = JSON.parse(data);
-      if (temp.stat === "verified") {
+      if (temp.stat === "1") {
         this.error = "";
-        this.info = "User already verified.";
+        this.login_info.setStorageInfo(temp.t, this.email, temp.ut);
+        if (temp.ut === "1")
+          this.router.navigate(['employer/' + this.email]);
+        else if (temp.ut === "2")
+          this.router.navigate(['employee/' + this.email]);
+        else if (temp.ut === "3")
+          this.router.navigate(['super/' + this.email]);
       }
       else if (temp.stat === "0") {
-        this.info = "";
         this.error = "Email Verification Unsuccessful.";
       }
-      else if (temp.stat === "1") {
-        this.error = "";
-        this.info = "Email Verification Successful."
-      }
-      this.login_info.setStorageInfo(temp.t, this.email, temp.ut);
-      if (temp.ut == "1")
-        this.router.navigate(['employer/' + this.email]);
-      else if (temp.ut == "2")
-        this.router.navigate(['employee/' + this.email]);
-      else if (temp.ut == "3")
-        this.router.navigate(['super/' + this.email]);
     }, error => {
-      this.info = "";
       this.error = "Could not verify due to some issues.";
     });
-
   }
-
-  public login() {
-    this.router.navigate(['/']);
-  }
-
 }
